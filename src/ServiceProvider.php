16ca89380d4ee1d070152b8780644ce11d1fe60b
Lee Hicks <leehicks@dreamfactory.com>
2017-02-07 12:33:49 -0500

    Fix registering in service providers.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,38 +8,42 @@ use DreamFactory\Core\Components\ServiceDocBuilder;
 use DreamFactory\Core\Enums\ServiceTypeGroups;
 use DreamFactory\Core\Services\ServiceManager;
 use DreamFactory\Core\Services\ServiceType;
+use Illuminate\Database\DatabaseManager;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     use ServiceDocBuilder;
 
-    public function boot()
+    public function register()
     {
-        $this->app->resolving('db', function ($db){
-            $db->extend('cassandra', function ($config){
+        $this->app->resolving('db', function (DatabaseManager $db) {
+            $db->extend('cassandra', function ($config) {
                 return new CassandraConnection($config);
             });
         });
 
         // Add our service types.
-        $this->app->resolving('df.service', function (ServiceManager $df){
+        $this->app->resolving('df.service', function (ServiceManager $df) {
             $df->addType(
                 new ServiceType([
-                    'name'           => 'cassandra',
-                    'label'          => 'Cassandra',
-                    'description'    => 'Database service for Cassandra connections.',
-                    'group'          => ServiceTypeGroups::DATABASE,
-                    'config_handler' => CassandraConfig::class,
+                    'name'            => 'cassandra',
+                    'label'           => 'Cassandra',
+                    'description'     => 'Database service for Cassandra connections.',
+                    'group'           => ServiceTypeGroups::DATABASE,
+                    'config_handler'  => CassandraConfig::class,
                     'default_api_doc' => function ($service) {
                         return $this->buildServiceDoc($service->id, Cassandra::getApiDocInfo($service));
                     },
-                    'factory'        => function ($config){
+                    'factory'         => function ($config) {
                         return new Cassandra($config);
                     },
                 ])
             );
         });
+    }
 
+    public function boot()
+    {
         // add migrations
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
